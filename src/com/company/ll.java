739a20856d17589968205b8e90a3801af4eae9cf
Petr <pdovgol@mail.ru>
2020-03-04 19:30:31 +0300

    list

--- src/com/company/ll.java
+++ src/com/company/ll.java
@@ -1,70 +1,114 @@
 package com.company;
 
-public class ll<T>
+public class ll<E>
 {
-    private node<T> first;
-    private node<T> last;
-    ll() {first = null;}
-    public void insert(T value)
+    // open JDK
+    transient int size = 0;
+    transient Node<E> first;
+    transient Node<E> last;
+
+    public ll()
     {
-        var newNode = createNewNode(value);
-        if (first == null)
-        {
-            first = newNode;
-            //last = first;
-        }
-        else
-        {
-            last.next = newNode;
+
+    }
+
+    private void linkFirst(E e)
+    {
+        final Node<E> f = first;
+        final Node<E> newNode = new Node<>(null, e, f);
+        first = newNode;
+        if (f == null)
             last = newNode;
+        else
+            f.prev = newNode;
+        size++;
+    }
+
+    private E unlinkFirst(Node<E> f) {
+        // assert f == first && f != null;
+        final E element = f.item;
+        final Node<E> next = f.next;
+        f.item = null;
+        f.next = null; // help GC
+        first = next;
+        if (next == null)
+            last = null;
+        else
+            next.prev = null;
+        size--;
+        return element;
+    }
+
+    E unlink(Node<E> x) {
+        // assert x != null;
+        final E element = x.item;
+        final Node<E> next = x.next;
+        final Node<E> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+        return element;
+    }
+
+    Node<E> node(int index) {
+        // assert isElementIndex(index);
+
+        if (index < (size >> 1)) {
+            Node<E> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            Node<E> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
         }
     }
 
-    public void eraseNode(int index)
-    {
-        nodeAt(index - 1).next = nodeAt(index + 1);
-    }
-
-    void printList()
-    {
-        var currentNode = this.first;
-        while (currentNode != null)
-        {
-            System.out.print(currentNode.data);
-            System.out.print("\n");
-            currentNode = currentNode.next;
-        }
-    }
-
-    public void insert(T value, int position)
-    {
-
-    }
-
-    public node<T> nodeAt(int index)
-    {
-        node<T> currentNode = this.first;
-        int currentIndex = 0;
-        for (int i = 0; i < index; ++i)
-            currentNode  = currentNode.next;
-        return currentNode;
-    }
-
-    private node<T> createNewNode(T val) { return new node<T>(val);}
-        class node<T> {
-            T data;
-            private node<T> next;
-            node(T d)
-            {
-                data = d;
+    public int indexOf(Object o) {
+        int index = 0;
+        if (o == null) {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (x.item == null)
+                    return index;
+                index++;
             }
-            public void setNext(node<T> next) {
-                this.next = next;
-            }
-
-            public node<T> getNext()
-            {
-                return next;
+        } else {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (o.equals(x.item))
+                    return index;
+                index++;
             }
         }
+        return -1;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
 }
+
+//
